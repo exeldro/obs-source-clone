7847739862b5a3a27887bf0547da38978c504b49
Exeldro <exelsor@hotmail.com>
2022-11-18 15:53:12 +0100

    sort source names

--- source-clone.c
+++ source-clone.c
@@ -115,8 +115,13 @@ void source_clone_defaults(obs_data_t *settings)
 bool source_clone_list_add_source(void *data, obs_source_t *source)
 {
 	obs_property_t *prop = data;
-	obs_property_list_add_string(prop, obs_source_get_name(source),
-				     obs_source_get_name(source));
+
+	const char * name = obs_source_get_name(source);
+	size_t count = obs_property_list_item_count(prop);
+	size_t idx = 0;
+	while(idx < count && strcmp(name, obs_property_list_item_string(prop, idx)) > 0)
+		idx++;
+	obs_property_list_insert_string(prop, idx, name, name);
 	return true;
 }
 
